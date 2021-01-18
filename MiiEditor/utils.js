0e74e1ab1c197cb05f9e416930857febb062f6a8
Draconic NEO <44508470+DraconicNEO@users.noreply.github.com>
2021-01-17 21:23:07 -0500

    Add files via upload

---
+++ MiiEditor/utils.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function getQueryObj(){var obj={};var query=window.location.search.substring(1);var pair,parts=query.split("&");var i,n=parts.length;for(i=0;i<n;i++){pair=parts[i].split("=");obj[pair[0]]=pair[1]||"1";}
+return obj;}
+function getBadgeParams(){return{appname:"My Avatar Editor",appurl:"http://"+window.location.hostname+"/downloads/MyAvatarEditor.air",airversion:1.5,imageurl:"/images/badge_preview.jpg"}}
+
+}
+/*
+     FILE ARCHIVED ON 17:43:12 Nov 07, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:59:33 Jan 16, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 369.1
+  exclusion.robots: 182.486
+  exclusion.robots.policy: 182.479
+  xauthn.identify: 118.407
+  xauthn.chkprivs: 63.875
+  RedisCDXSource: 1.952
+  esindex: 0.008
+  LoadShardBlock: 168.79 (3)
+  PetaboxLoader3.datanode: 121.297 (4)
+  CDXLines.iter: 13.82 (3)
+  PetaboxLoader3.resolve: 106.328 (2)
+  load_resource: 268.085
+*/
